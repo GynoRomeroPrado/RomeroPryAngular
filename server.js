e9a975f853b675c8696046fc07f015ea8a135662
gyno paolo romero prado <gromero@pipersolutions.com>
2020-11-25 01:37:00 -0500

    server3.5

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
   const express = require('express');
-
+  const path = require('path');
   const app = express();
 
-  app.use(express.static('./dist/'));
+  app.use(express.static('./dist/romeropryangular'));
 
   app.get('/*', (req, res) =>
-      res.sendFile('index.html', { root: 'dist/' }),
+      res.sendFile('index.html', { root: 'dist/romeropryangular/' }),
+
   );
 
   app.listen(process.env.PORT || 8080);
