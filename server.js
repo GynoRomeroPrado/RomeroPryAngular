0e6006b5d6d6135dfffa5a18fe099e8870ca0f3d
GynoRomeroPrado <70270633+GynoRomeroPrado@users.noreply.github.com>
2020-11-25 02:17:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,17 @@
   const app = express();
 
   //app.use(express.static('./dist/romeropryangular'));
-  //app.use(express.static(__dirname + '/dist'));
+  /*app.use(express.static(__dirname + '/dist'));
 
   app.get('/*', (req, res) =>
       const index = path.join(__dirname, 'build', 'index.html');
       res.sendFile(index);
   );
+*/
+  app.use(express.static(__dirname + '/dist/romero-pry-angular'));
+
+  app.get('/*', function(req,res) {
+      res.sendFile(path.join(__dirname+'/dist/romero-pry-angular/index.html'));
+  });
 
   app.listen(process.env.PORT || 8080);
